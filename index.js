1649ccdb6f5a03f034bbcbff4bf714849bdccb4c
Kkrisi <kadarkrisi@gmail.com>
2024-03-27 13:01:35 +0100

    fuggvenyek letrehozva

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-function negyzetgeneralas(){
-    for ()
+window.addEventListener("load", function () {
+    //NegyzetGeneralas();
+    TombLetrhoz();
+    Kattintas();
+   
+});
+
+
+
+
+
+
+
+function TombLetrhoz(){
+    const Tomb = [];
+    for(let i = 0; i < 25; i++){
+        Tomb[i] = i;
+    }
 }
+
+
+const NEGYZET = $("kocka"); 
+
+
+function Kattintas(){
+    for (let i = 0; i < Negyzet.length; i++) {
+        Negyzet[i].on
+        eq(0);
+            if (Negyzet[i].textContent.){
+                //classAdd
+            } else if(jatekos === 0){
+
+            } else {
+
+            }
+            console.log("jatekos, lepesek: " + jatekos + "   " + lepesek);
+            //if(JatekVege()){};
+        });
+      }
+}
+
+
+
+//function JatekVege(){}
