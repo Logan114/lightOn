3a5db9d6183fd56b7e1dc8ea95f8dae83a8d97a3
Kkrisi <kadarkrisi@gmail.com>
2024-03-27 13:16:13 +0100

    negyzetek megjelentek

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-window.addEventListener("load", function () {
-    //NegyzetGeneralas();
-    TombLetrhoz();
-    Kattintas();
+$("load", function () {
+    negyzetGeneralas();
+    tombLetrhoz();
+    kattintas();
    
 });
 
@@ -10,8 +10,20 @@ window.addEventListener("load", function () {
 
 
 
+function negyzetGeneralas(){
+    const mezo = $("#palya")
 
-function TombLetrhoz(){
+    for (let i=0; i<25; i++){
+        mezo.append(`<div class="kocka${i} kocka"><p>${i}</p><br></div>`);
+        
+    }
+}
+
+
+
+
+
+function tombLetrhoz(){
     const Tomb = [];
     for(let i = 0; i < 25; i++){
         Tomb[i] = i;
@@ -19,35 +31,22 @@ function TombLetrhoz(){
 }
 
 
+
 const NEGYZET = $("kocka"); 
 
-
+/*
 function Kattintas(){
-    for (let i = 0; i < Negyzet.length; i++) {
-        Negyzet[i].on
-        eq(0);
-            if (Negyzet[i].textContent.){
+        NEGYZET.on("click", function(){
+            if (){
                 //classAdd
-            } else if(jatekos === 0){
-
-            } else {
+            }  else {
 
             }
-            console.log("jatekos, lepesek: " + jatekos + "   " + lepesek);
             //if(JatekVege()){};
         });
-      }
 }
-
+*/
 
 
 //function JatekVege(){}
-function negyzetgeneralas(){
-    const mezo = $(".palya")
 
-    for (let i=0; i<25; i++){
-        $(mezo).append(`<div class="kocka${i} kocka"><p>${i}</p><br></div>`);
-        
-    }
-}
-negyzetgeneralas();
