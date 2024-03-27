376a6c77937115979b8a44f73253fdab2cff19cd
Logan114 <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>
2024-03-27 13:18:05 +0100

    theCrab

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ function TombLetrhoz(){
 
 
 const NEGYZET = $("kocka"); 
+function szinValtas(elem){
+    $(elem).css("background","hotpink")
+}
 
-
+/*
 function Kattintas(){
     for (let i = 0; i < Negyzet.length; i++) {
         Negyzet[i].on
@@ -35,19 +38,19 @@ function Kattintas(){
             }
             console.log("jatekos, lepesek: " + jatekos + "   " + lepesek);
             //if(JatekVege()){};
-        });
+        };
       }
 }
 
-
+*/
 
 //function JatekVege(){}
 function negyzetgeneralas(){
-    const mezo = $(".palya")
+    const Mezo = $("#palya")
 
     for (let i=0; i<25; i++){
-        $(mezo).append(`<div class="kocka${i} kocka"><p>${i}</p><br></div>`);
+        $(Mezo).append(`<div class="kocka${i} kocka"><p>${i}</p><br></div>`);
         
     }
 }
-negyzetgeneralas();
+negyzetgeneralas()
